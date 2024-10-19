edd34e3f15f6436fb87acf5d6545154bdfbfa967
Masha <klimova_v04@bk.ru>
2024-10-19 23:36:35 +0700

    Итерация 1: Объявлены все классы. Все основные методы в виде заглушек

---
+++ text_analyzer.py
@@ -0,0 +1,28 @@
+ class TextAnalyzer:
+    def __init__(self):
+        pass
+    
+    def input_data(self):
+        return "образец"
+    
+    def execute_algorithm(self):
+        pass
+    
+    def output_result(self):
+        pass
+
+class Menu:
+    def __init__(self):
+        pass
+    
+    def display(self):
+        pass
+    
+    def handle_choice(self):
+        pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main() 
